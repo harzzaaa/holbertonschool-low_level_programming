bb374c96e29a04a0aee2c54f803b1c6e791ecbe1
harzzaaa <hepar79@protonmail.com>
2024-06-17 09:32:57 +0000

    HelloPuts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main
- *return 0
+ *main - Entry Point
+ *return 0 (Success)
 */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
