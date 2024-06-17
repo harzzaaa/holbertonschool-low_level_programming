3e1aa61a413340c89c9bcbafd776c1d367e71351
harzzaaa <hepar79@protonmail.com>
2024-06-17 08:56:46 +0000

    Hello Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-gcc -o $CFILE "Programming is like building a multilingual puzzle
+#include <stdio.h>
+int main()
+{
+	char* str1 = "Programming is like building a multilingual puzzle";
+	puts(str1);
+
+	puts("Programming is like building a multilingual puzzle");
+
+	return 0;	
