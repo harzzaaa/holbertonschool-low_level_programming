5bc4d12241ef43d7590ff466dd89cb250998ab5d
harzzaaa <hepar79@protonmail.com>
2024-06-18 12:19:03 +0000

    AlphabetLower&Upper

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,6 +3,21 @@
 #include <time.h>
 
 /**
- * main - 
- * Return: 
+ * main - Prints alphabet in lowercase then uppercase
+ * Return: Alphabet in lowercase and uppercase
 */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
