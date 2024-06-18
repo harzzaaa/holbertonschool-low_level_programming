062703ba3e1d15101ea5b76f08b605236de63f8d
harzzaaa <hepar79@protonmail.com>
2024-06-18 10:00:38 +0000

    LastDigit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints last digits Random Num
+ * Return: Last number
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("Last digit of number %d \n", n);  
+	return (0);
+}
