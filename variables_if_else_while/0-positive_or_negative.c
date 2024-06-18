17c259b9590d14183e9cd9f7be3338642193a05d
harzzaaa <hepar79@protonmail.com>
2024-06-18 09:06:20 +0000

    +&-

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,10 @@
 #include <time.h>
 
 /**
- *main - Print random number each time the script is executed
- *Return: response
+ * main - Print random number each time the script is executed
+ * Return: response
  */
+
 int main(void)
 {
 	int n;
@@ -14,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
