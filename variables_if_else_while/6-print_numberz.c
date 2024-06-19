3dc5fcb164583442840646ce70876fdb38fac269
harzzaaa <hepar79@protonmail.com>
2024-06-19 10:03:42 +0000

    NumberZ

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints single digit number
+ * Return: Numbers
+*/
+
+int main(void)
+{
+	int 1;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
