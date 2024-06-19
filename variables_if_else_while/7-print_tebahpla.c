1ef78e52761ff5ef3996e8d2cde38fe80530dfb3
harzzaaa <hepar79@protonmail.com>
2024-06-19 11:11:57 +0000

    SmileInTheMirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints Alphabet lowercase & reverse
+ * Return: Reversed Alphabet in Lowercase
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; i++)
+
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
