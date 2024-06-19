8ba3edb9b8755f3467e6339a4b0e30ff458ea561
harzzaaa <hepar79@protonmail.com>
2024-06-19 18:45:07 +0000

    PrintComb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,6 +3,23 @@
 #include <time.h>
 
 /**
- * main -
- * Return: 
-*/ 
+ * main - Prints all
+ * Return: Single digit
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		} else
+			putchar('\n');
+	}
+	return (0);
+}
