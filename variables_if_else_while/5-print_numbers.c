8192b467524ffdaeaaba0f520cce0e03b8c7539b
harzzaaa <hepar79@protonmail.com>
2024-06-19 08:49:16 +0000

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints single digit numbers
+ * Return: 0 to 9
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf ("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
