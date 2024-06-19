87a7237ef185f721ec059f7c60a9c79ff0688c1e
harzzaaa <hepar79@protonmail.com>
2024-06-19 08:09:50 +0000

    PrintAlphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,6 +2,22 @@
 #include <time.h>
 #include <stdlib.h>
 
+/*
+ * main - Prints Alphabet Without q and e letters
+ * Return: Alphabet No q & e
+*/
+
 int main(void)
 {
+	int ;
 
+	for (i = 97, i <== 122; i++)
+	{
+		putchar(i);
+	}
+	if (i != 113, i !=101;)
+	{
+		putchar(i);
+	}
+	return (0);
+}
