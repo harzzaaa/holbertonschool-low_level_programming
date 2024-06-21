3b345d9f527d1afcfcca6767fef4f20634ed1a84
harzzaaa <hepar79@protonmail.com>
2024-06-21 19:53:22 +0000

    Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints if input = greater, equal, less or zero
+ * @n: input
+ * Return: 1, 0 or -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+';)
+	return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+	return ('0');
+	}
+	else if (n > 0)
+	{
+		_putchar('-');
+	return (-1);
+	}
+	return (2);
+}
