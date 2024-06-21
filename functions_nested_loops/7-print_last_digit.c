345267c0f9a1394050961668bd3866eb1511780f
harzzaaa <hepar79@protonmail.com>
2024-06-21 21:09:52 +0000

    PrintLastDigit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit 
+ * @num: input
+ * Return: last digit of input 
+*/
+
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	if (num < 0)
+	last_digit *= -1;
+
+	_putchar (last_digit + 0);
+	return (last_digit);
