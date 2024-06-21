997a2ef26170ecb4bf5758072f2401fa255c0095
harzzaaa <hepar79@protonmail.com>
2024-06-21 11:55:44 +0000

    Alphabetx10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * print_alphabet_x10 - Print alphabet X 10
+*/
+
+void print_alphabet_x10(void)
+{
+	char alphabetX10;
+{
+	for (alphabetX10 = 'a'; alphabetX10 <= 'z'; alphabetX10++)
+		_putchar (alphabet10);
+}
+	_putchar('\n')
+}
