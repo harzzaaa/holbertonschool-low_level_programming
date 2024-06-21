812ab532731e8a22ded459d442d3b47045acd383
harzzaaa <hepar79@protonmail.com>
2024-06-21 09:00:43 +0000

    _putchar

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,12 @@
 	void _print_alphabet_x10(void);
 	int _islower(int c);
 	int _isalpha(int c);
-#endif 
+	int print_sign(int n);
+	int _abs(int);
+	int print_last_digit(int);
+	void (int);
+	void (int);
+	int (int, int);
+	void (int);
+	void (int);
+#endif
