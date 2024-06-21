5819ad1be7ffa2445b7ad2d47122d754d22ab873
harzzaaa <hepar79@protonmail.com>
2024-06-21 19:11:31 +0000

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check entry lowercase or not
+ * @c : entry check
+ * Return: a number
+*/
+
+int _islower(int c);
+{
+	if (c > 97 && c < 124)
+		return (1);
+	else if
+		return (0);
+}
