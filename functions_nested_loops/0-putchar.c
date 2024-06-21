0e76332ff42742e2ab7862ccc7aae5666a0cf5ac
harzzaaa <hepar79@protonmail.com>
2024-06-21 08:14:13 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /*
- * main - 
- * Return:
+ * main - Print _putchar + new line
+ * Return: _putchar
 */ 
 
 int main(void)
-{	
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
