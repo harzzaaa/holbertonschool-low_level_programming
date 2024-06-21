6a58e9d27e17ba27531c5cfde141333e8c41e94c
harzzaaa <hepar79@protonmail.com>
2024-06-21 12:03:16 +0000

    X10Alphabet

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,13 +2,6 @@
 #define MAIN
 
 int _putchar(char c);
-void _print_alphabet(void);
-void _print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 #endif
