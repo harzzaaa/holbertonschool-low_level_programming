c6f982b5902a0731b695451058697a597155c331
harzzaaa <hepar79@protonmail.com>
2024-06-21 19:41:04 +0000

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _isalpha - check input as alpha
+ * @c: input to check
+ * Return: 1 if input is alphabet or no
 */
 
 int _isalpha(int c)
 {
-	if (c > 64 && c < 91) | (c > 96 && c < 123)
-		return(0);
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
 	else
-		return(1);
+		return (0);
 }
