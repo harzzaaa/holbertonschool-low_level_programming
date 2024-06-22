743d6d318f5debab9ea31c96066a4418b1bc8ffc
harzzaaa <hepar79@protonmail.com>
2024-06-22 18:37:52 +0000

    a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function who adds two two integers
+ * @m: 1st integer
+ * @n: 2sd integer
+ * Return: Result of combined @m & @n
+*/
+
+int add(int m, int n)
+{
+	return(m + n);
+}
