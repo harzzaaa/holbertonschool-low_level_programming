76784cabacee502be1a03a715c0f6111acc332bd
harzzaaa <hepar79@protonmail.com>
2024-06-24 11:29:48 +0000

    PrintNumbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	char numbers;
 	{
-		for (numbers = 0 ; numbers <= 9; numbers++)
+		for (numbers = '0' ; numbers <= '9' ; numbers++)
 
-			_putchar(numbers);
+			_putchar (numbers);
 	}
 	_putchar('\n');
 }
