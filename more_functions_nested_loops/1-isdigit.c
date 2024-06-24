a675aec5faee31030bd4fd48083e25ad956ad829
harzzaaa <hepar79@protonmail.com>
2024-06-24 10:23:05 +0000

    IsDigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isdigit - Check for A digit.
- * c : Name used to refer _isdigit prototype
- * return: 1 if c is a digit otherwise 0
+ * @c : Name used to refer _isdigit prototype
+ * Return: 1 if c is a digit otherwise 0
 */
 
 int _isdigit(int c)
