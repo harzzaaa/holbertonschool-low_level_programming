2029ce3ed4f9d5fe0193d8ed44f4ee8834545548
harzzaaa <hepar79@protonmail.com>
2024-06-24 12:31:36 +0000

    PrintMostNumbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers 0 to 9
+ * Return: Print 0 to 9 but not 2 & 4
+*/
+
+void print_most_numbers(void)
+{
+	char numbers;
+	{
+		for (numbers = '0' ; numbers <= '9' ; numbers++)
+			_putchar (numbers != '2' &&  numbers != '4');
+	}	
+		_putchar('\n');
+	
+}
