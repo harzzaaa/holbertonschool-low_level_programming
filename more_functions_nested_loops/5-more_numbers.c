1a0dc8f3f049e0c42fa62527964b4e073265bfb1
harzzaaa <hepar79@protonmail.com>
2024-06-24 16:44:16 +0000

    10x014

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+*/
+
+void more_numbers(void)
+{
+	int m;
+	char morenumbers;
+		{
+		for (m = 0; m < 10; m++)
+			_putchar(morenumbers = 0 morenumbers <= 14)
+			_putchar(morenumbers)
+		}
+	_putchar('\n');
+}
