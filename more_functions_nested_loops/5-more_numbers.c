e00d195aad78777fe49b78cd9ba08f2d5da0bbe2
harzzaaa <hepar79@protonmail.com>
2024-06-24 18:39:06 +0000

    10x014

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,21 @@
 void more_numbers(void)
 {
 	int h, m;
+	char newdigit;
 
 	for (h = 0; h < 10; h++)
 	{
 		for (m = 0; m <= 14; m++)
 		{
 			if (m > 10)
-			_putchar(m + '0');
-			_putchar(' ');
+				newdigit = m + '0';
+			else
+				newdigit = '1';
+			
+			_putchar(newdigit);
+			
+			if (m < 14)
+				_putchar(' ');
 		}
 	_putchar('\n');
 	}
