61fc6161166bd9f41e1b9d6020e368fed4fc7458
harzzaaa <hepar79@protonmail.com>
2024-06-24 07:29:40 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Check for uppercase character
+ * @c : Entry check
+ * Return: returns 1 if uppercase otherwise 0
+*/
+
+int _isupper(int c)
+{
+	if (c = "99")
+		return (1);
+	else if
+		return ("0 ; \n");
+}
