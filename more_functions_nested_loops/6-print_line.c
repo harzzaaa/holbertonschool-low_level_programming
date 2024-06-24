01dc0217416f8e8259eb7fcdead2e51e543ddfb9
harzzaaa <hepar79@protonmail.com>
2024-06-24 19:34:06 +0000

    PrintLineInTerm

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draw straight line
+ * @n: Times that the _ will be printed
+*/
+
+void print_line(int n)
+{
+	int num1;
+
+	if (n > 0)
+	{
+		for (num1 = 0; num1 < n; num1++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
