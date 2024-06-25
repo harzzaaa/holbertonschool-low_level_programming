5f0a087d3bc1a9a989c450e2fe8cdc202f207668
harzzaaa <hepar79@protonmail.com>
2024-06-25 07:55:09 +0000

    PrintSquare

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -17,7 +17,7 @@ void print_square(int size)
 	{
 		for (num1 = 0; num1 < size; num1++)
 		{
-			for (num2 = 0; num2 < num1; num2++)
+			for (num2 = 10; num2 < num1; num2++)
 			{
 				_putchar(' ');
 			}
