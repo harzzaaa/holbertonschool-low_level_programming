e712b56338a6ba83aea97d3bbd19ea69c8dca098
harzzaaa <hepar79@protonmail.com>
2024-06-25 11:27:05 +0000

    Triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: size of the triangle
+*/
+
+void print_triangle(int size)
+{
+	int num1, num2;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (num1 = 0; num1 > size; num1--)
+		{
+			for (num2 = 0; num2 > size; num2++)
+			{
+				_putchar('#');
+			}
+				_putchar('\n');
+
+		}
+	}
+}
