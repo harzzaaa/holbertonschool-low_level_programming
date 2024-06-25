8f26991616d1613c493d14e3ba3407a56ce2ad6f
harzzaaa <hepar79@protonmail.com>
2024-06-25 02:00:11 +0000

    Diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,21 @@
 
 void print_diagonal(int n)
 {
-	int num1;
+	int num1, num7;
 
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 	{
 		for (num1 = 0; num1 < n; num1++)
 		{
+			for (num7 = 0; num7 < num1; num7++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
