0151ccadb5448abb0fb407cdf30a2324e8e886be
harzzaaa <hepar79@protonmail.com>
2024-06-25 11:08:12 +0000

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,16 @@ int main(void)
 	for (noon = 1; noon <= 100; noon++)
 	{
 		if (noon % 3 == 0 && noon % 5 == 0)
-
 			printf("FizzBuzz");
 
 		else if (noon % 3 == 0)
-
 			printf("Fizz");
 
 		else if (noon % 5 == 0)
-
 			printf("Buzz");
 
 		else
-		printf("%d ", noon);
+			printf("%d ", noon);
 
 		if (noon != 100)
 			printf(" ");
