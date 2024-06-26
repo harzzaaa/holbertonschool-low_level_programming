67df77f13bf2288f3cf256050660e712803044ac
harzzaaa <hepar79@protonmail.com>
2024-06-26 18:32:54 +0000

    Puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,10 +3,28 @@
 
 /**
  *_puts - function that print string
- *
- *
+ *@str: string that going to be printed
+ *Return: return to void
 */
 
 void _puts(char *str)
 {
-		
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
+/**
+ *main - test of _puts function 
+ *
+ *Return: 0 
+*/  
+int main(void)
+{
+	char string = "Welcome Back";
+       _puts(string);
+	return (0);
+}
