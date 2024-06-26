cbf5a5b3888a508f6d7b5fed4ff7d12c923d1ede
harzzaaa <hepar79@protonmail.com>
2024-06-26 10:55:38 +0000

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -9,8 +9,19 @@
 
 void swap_int(int *a, int *b)
 {
-	*a = 98;
-	*b = 42;
-
-	swap_int(a, b);
+	int err = *a;
+	*a = *b;
+	*b = err;
 }
+int main(void)
+{
+	int a = 98;
+	int b = 42;
+
+	printf("a = %d, b = %d\n", a, b);
+	swap_int(&a, &b);
+	printf("a = %d, b = %d\n", a, b);
+
+	return ('0');
+}
+
