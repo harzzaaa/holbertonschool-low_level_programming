75b0fac9ef9e42cb49d92bea5137c25c1fe156f9
harzzaaa <hepar79@protonmail.com>
2024-06-26 07:23:39 +0000

    ResetTo98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+*/
+
+void reset_to_98(int *n)
+{
+	int o = 98;
+	int *ptr = &n;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
+int _putchar(int c);
+void reset_to_98(int *n);
+#endif
