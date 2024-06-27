2cc5632d667f04bf11d080ab7f43df71f97e87ba
harzzaaa <hepar79@protonmail.com>
2024-06-27 13:55:47 +0000

    RevString

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - function that going to reverse a string
+ * main - reverse a string
+ * @s: ?
+ * Return: 0
+*/
+
+void rev_string(char *s);
+{
+	int l = 0;
+	int i;
+	char pmet;
+
+
+
+	_putchar(s[l]);
+
+	}
+
+	_putchar('\n');
+}
