e06ac8e1f5f53a5e5b9376c6564cd920a565932a
harzzaaa <hepar79@protonmail.com>
2024-06-27 19:50:47 +0000

    ReverseStringForReal

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * rev_string - function that going to reverse a string
  * main - reverse a string
@@ -7,13 +7,19 @@
  * Return: 0
 */
 
-void rev_string(char *s);
+void rev_string(char *s)
 {
 	int l = 0;
-	int i;
+	int i = strlen(s) - 1;
 	char pmet;
 
-
+	while (l < i)
+	{
+		pmet = s[];
+		s[] = s[];
+		s[] = pmet;
+		l++;
+		i++;
 
 	_putchar(s[l]);
 
