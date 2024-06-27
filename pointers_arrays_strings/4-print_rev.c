f2be974d2856d09b77e8177198dcf936575d0973
harzzaaa <hepar79@protonmail.com>
2024-06-27 11:08:38 +0000

    ReversedString

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -19,23 +19,7 @@ void print_rev(char *s)
 
 	for (m = longueur - 1; m >= 0; m--)
 	{
-		printf("%d", s[m]);
+		printf("%c", s[m]);
 	}
 	printf("\n");
-}
-
-/**
- *main1 - test the print_rev function
- *Return: 0
-*/
-
-int main1(void)
-{
-	{
-	char reversestring[] = "Reverse String For?";
-
-	print_rev(reversestring);
-
-	}
-	return (0);
 }
