e142b0f4e683a23dcebf64113fd86c2be6c8a0d7
harzzaaa <hepar79@protonmail.com>
2024-06-30 18:10:22 +0000

    StrnCat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ * _strncat - Function that concatenates two strings
+ * @dest: string
+ * @src: string to *dest
+ * @n: byte of src's string
+ *Return: (dest)
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *pointer = dest
+	char *pointer = dest;
 	int i = 0;
 
 	while (*pointer != '\0')
@@ -17,13 +18,13 @@ char *_strncat(char *dest, char *src, int n)
 	pointer++;
 	}
 
-		while (i < n && src [i] != '\0')
+		while (i < n && src[i] != '\0')
 		{
 		*pointer = src[i];
 		pointer++;
 		i++;
 		}
-		
+
 		*pointer = 0;
 	return (dest);
-}	
+}
