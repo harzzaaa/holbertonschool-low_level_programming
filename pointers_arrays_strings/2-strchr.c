4457ca5097b906075b4e1bd76f266bfe2661ada7
harzzaaa <hepar79@protonmail.com>
2024-07-01 12:28:08 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Function that locates a character in the string
+ *@s: main string
+ *@c: occurrence
+ *Return: NULL if not found
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	{
+	s++;
+	}
+
+	return NULL;
+}
