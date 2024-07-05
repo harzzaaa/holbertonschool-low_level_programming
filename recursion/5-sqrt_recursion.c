db5a11332615634c4387948577e8e869173380b6
harzzaaa <hepar79@protonmail.com>
2024-07-05 02:25:16 +0000

    SqrtRec

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Function who returns natural square root of a number
- * @n: natural square root~
+ * helping - Search square root using rec'
+ * @n: natural square root search
+ * @i: value of square root
  * Return: Function
 */
+
+int helping(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (helping(n, i + 1));
+}
+/**
+ * _sqrt_recursion - Send square root natural
+ * @n: number that we search square root
+ * Return: square root of n or -1 if does not exit
+ */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-
-	return (n);
+		return (helping(n, 0));
 }
