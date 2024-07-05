83cd8d01cf47103488310693e7048abce5df43a4
harzzaaa <hepar79@protonmail.com>
2024-07-05 16:57:35 +0000

    WhatsMyName

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints and return to a new line
+ *@argc: argument
+ *@argv: argument
+ * Return: file
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
