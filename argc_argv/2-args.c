991ddbe13e77a0b39e886ab74783675296937add
harzzaaa <hepar79@protonmail.com>
2024-07-05 21:28:25 +0000

    mul

---
+++ argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - program that print arguments
+ *@argc: argument
+ *@argv: argument
+ * Return: string a to argv
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a <
