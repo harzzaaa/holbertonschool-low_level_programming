6f6d7d025a36db719fa58c5dbb96ad6b4832a095
harzzaaa <hepar79@protonmail.com>
2024-07-12 12:57:34 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef doggy
+#define doggy
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - structure for dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ *typedef dog_d - name of struct
+*/
+
+typedef struct dog dog_d;
+
+void init_dog(struct dog *p, char *name, float age, char *owner);
+void print_dog(struct dog *p);
+void free_dog(dog_d *p);
+#endif
