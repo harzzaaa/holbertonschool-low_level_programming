935122560e54461866acafc343184950d2ec783b
harzzaaa <hepar79@protonmail.com>
2024-07-16 09:38:34 +0000

    inTIndex

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function who searches for an integer
+ * @array: ptr to array of ints
+ * @size: nmb of elements in array
+ * @cmp: ptr to function
+ * Return: 0 or -1
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int m;
+
+	if (size <= 0)
+	return (-1);
+
+	for (m = 0; m < size; m++)
+	{
+		if (cmp(array[m]) != 0)
+			return (m);
+	}
+
+	return (-1);
+}
