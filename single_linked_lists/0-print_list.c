5942ae02427e91140232c9d41c58116edc7aa25e
harzzaaa <hepar79@protonmail.com>
2024-07-29 09:25:34 +0000

    Task 0 Completed

--- single_linked_lists/0-print_list.c
+++ single_linked_lists/0-print_list.c
@@ -17,14 +17,14 @@ size_t print_list(const list_t *h)
 	{
 	if (h->str == NULL)
 	{
-		printf("[0] (nil)");
+		printf("[0] (nil)\n");
 	}
 	else
 	{
 		printf("[%u] %s\n", h->len, h->str);
 	}
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
