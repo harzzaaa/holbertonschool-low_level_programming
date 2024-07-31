3345d6407d2de9bbdc4bd0b6424e4a93da946193
harzzaaa <hepar79@protonmail.com>
2024-07-31 09:30:41 +0000

    Task 4 Completed

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
+ * free_list - Function that frees the list_t list
  *
- *
- *
- *
+ * @head: Pointer that points to the head of the list
 */
 
 void free_list(list_t *head)
 {
+	list_t *cur = head;
+
+	while (cur != NULL)
+	{
+		list_t *node = cur->next;
+
+		if (cur->str != NULL)
+	{
+		free(cur->str);
+	}
+	free(cur);
+	cur = node;
+	}
+}
