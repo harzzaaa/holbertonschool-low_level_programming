4b234395e6cc5983f6a7b1e172d6697de2f3a5d6
harzzaaa <hepar79@protonmail.com>
2024-08-16 22:27:10 +0000

    0

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,43 +1,47 @@
 #include "lists.h"
 
 /**
- * add_node - Function who adds a new node at the start of list_t list
+ * add_node - Function who adds a new node
  *
- * @head: Double pointer to node
+ * @str:
  *
- * @str: Pointer string in new node
+ * @head:
  *
- * Return: Returns address or NULL if no memory
+ * Return: Address of new element
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *my_node;
-	unsigned int lenght;
+	list_t *thenode
+	unsigned int length;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	my_node = malloc(sizeof(list_t));
-	if (my_node == NULL)
+	thenode = (list_t *)malloc(sizeof(list_t));
+	if (thenode == NULL)
+
 	{
+			return (NULL);
+	}
+
+	thenode->str = strdup(str);
+	if (thenode->str == NULL)
+	{
+		free(thenode);
 		return (NULL);
 	}
-	my_node->str = strdup(str);
-	if (my_node->str == NULL)
+	
+	length = 0;
+	while (str[length] != '\0')
 	{
-	free(my_node);
-	return (NULL);
+		length++;
 	}
-	lenght = 0;
-	while (str[lenght] != '\0')
-		lenght++;
+	thenode->len = length;
+	thenode->next = *head;
+	*head = thenode;
 
-	my_node->len = lenght;
-	my_node->next = *head;
-	*head = my_node;
-
-	return (my_node);
+	return (thenode);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,55 +1,13 @@
 #include "lists.h"
 
 /**
- * add_node_end - Function that adds a new node at the end of the list.
+ * add_node_end - Function who will add a new node at the end of a list_t list
  *
- * @str: s
  *
- * @head: h
  *
- * Return: End List
+ *
 */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *my_node, *last_node;
-	unsigned int lenght;
 
-	if (str == NULL)
-	{
-	return ('\0');
-	}
-	my_node = malloc(sizeof(list_t));
-
-	if (my_node == NULL)
-	{
-	return ('\0');
-	}
-	my_node->str = strdup(str);
-	if (my_node->str == NULL)
-	{
-		free(my_node);
-		return ('\0');
-	}
-	lenght = 0;
-
-	while (str[lenght] != '\0')
-		lenght++;
-	my_node->len = lenght;
-	my_node->next = NULL;
-	if (*head == NULL)
-	{
-		*head = my_node;
-	}
-	else
-	{
-		last_node = *head;
-
-		while (last_node->next != NULL)
-		{
-			last_node = last_node->next;
-		}
-		last_node->next = my_node;
-	}
-	return (my_node);
-}
